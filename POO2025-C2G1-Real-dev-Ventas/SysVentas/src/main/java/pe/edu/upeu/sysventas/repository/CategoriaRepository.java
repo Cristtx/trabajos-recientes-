3e52d351dd9d87a2aa96d7975c2462a8034f7805
Cristtx <indujoel789@gmail.com>
2025-10-10 16:20:48 -0500

    Add files via upload

---
+++ POO2025-C2G1-Real-dev-Ventas/SysVentas/src/main/java/pe/edu/upeu/sysventas/repository/CategoriaRepository.java
@@ -0,0 +1,7 @@
+package pe.edu.upeu.sysventas.repository;
+
+import pe.edu.upeu.sysventas.model.Categoria;
+
+public interface CategoriaRepository extends ICrudGenericRepository<Categoria, Long> {
+
+}
---
+++ POO2025-C2G1-Real-dev-Ventas/SysVentas/src/main/java/pe/edu/upeu/sysventas/repository/CompCarritoRepository.java
@@ -0,0 +1,6 @@
+package pe.edu.upeu.sysventas.repository;
+
+import pe.edu.upeu.sysventas.model.Producto;
+
+public interface CompCarritoRepository extends ICrudGenericRepository<Producto,Long>{
+}
